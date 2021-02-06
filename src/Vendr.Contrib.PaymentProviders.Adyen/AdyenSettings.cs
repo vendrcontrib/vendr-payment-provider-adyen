e56bd3d4cf7e7424054be56e5962b6be93e177b5
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2021-02-06 15:01:18 +0100

    Rename project

--- src/Vendr.Contrib.PaymentProviders.Adyen/AdyenSettings.cs
+++ src/Vendr.Contrib.PaymentProviders.Adyen/AdyenSettings.cs
@@ -1,8 +1,8 @@
 ﻿using Vendr.Core.Web.PaymentProviders;
 
-namespace Vendr.Contrib.PaymentProviders.Template
+namespace Vendr.Contrib.PaymentProviders.Adyen
 {
-    public class TemplateSettings
+    public class AdyenSettings
     {
         [PaymentProviderSetting(Name = "Continue URL", Description = "The URL to continue to after this provider has done processing. eg: /continue/")]
         public string ContinueUrl { get; set; }
