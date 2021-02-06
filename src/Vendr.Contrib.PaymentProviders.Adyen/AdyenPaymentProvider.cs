f953d406092f019390c7c83c0cfb900723772531
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2021-02-06 15:01:18 +0100

    Rename project

--- src/Vendr.Contrib.PaymentProviders.Adyen/AdyenPaymentProvider.cs
+++ src/Vendr.Contrib.PaymentProviders.Adyen/AdyenPaymentProvider.cs
@@ -6,18 +6,18 @@ using Vendr.Core.Models;
 using Vendr.Core.Web.Api;
 using Vendr.Core.Web.PaymentProviders;
 
-namespace Vendr.Contrib.PaymentProviders.Template
+namespace Vendr.Contrib.PaymentProviders.Adyen
 {
-    [PaymentProvider("template", "Template", "Template payment provider", Icon = "icon-invoice")]
-    public class TemplatePaymentProvider : PaymentProviderBase<TemplateSettings>
+    [PaymentProvider("adyen", "Adyen", "Adyen payment provider", Icon = "icon-invoice")]
+    public class AdyenPaymentProvider : PaymentProviderBase<AdyenSettings>
     {
-        public TemplatePaymentProvider(VendrContext vendr)
+        public AdyenPaymentProvider(VendrContext vendr)
             : base(vendr)
         { }
 
         public override bool FinalizeAtContinueUrl => true;
 
-        public override PaymentFormResult GenerateForm(OrderReadOnly order, string continueUrl, string cancelUrl, string callbackUrl, TemplateSettings settings)
+        public override PaymentFormResult GenerateForm(OrderReadOnly order, string continueUrl, string cancelUrl, string callbackUrl, AdyenSettings settings)
         {
             return new PaymentFormResult()
             {
@@ -25,17 +25,17 @@ namespace Vendr.Contrib.PaymentProviders.Template
             };
         }
 
-        public override string GetCancelUrl(OrderReadOnly order, TemplateSettings settings)
+        public override string GetCancelUrl(OrderReadOnly order, AdyenSettings settings)
         {
             return string.Empty;
         }
 
-        public override string GetErrorUrl(OrderReadOnly order, TemplateSettings settings)
+        public override string GetErrorUrl(OrderReadOnly order, AdyenSettings settings)
         {
             return string.Empty;
         }
 
-        public override string GetContinueUrl(OrderReadOnly order, TemplateSettings settings)
+        public override string GetContinueUrl(OrderReadOnly order, AdyenSettings settings)
         {
             settings.MustNotBeNull("settings");
             settings.ContinueUrl.MustNotBeNull("settings.ContinueUrl");
@@ -43,7 +43,7 @@ namespace Vendr.Contrib.PaymentProviders.Template
             return settings.ContinueUrl;
         }
 
-        public override CallbackResult ProcessCallback(OrderReadOnly order, HttpRequestBase request, TemplateSettings settings)
+        public override CallbackResult ProcessCallback(OrderReadOnly order, HttpRequestBase request, AdyenSettings settings)
         {
             return new CallbackResult
             {
