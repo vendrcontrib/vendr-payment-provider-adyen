551b37f892e4bda1b2e5e93cba7b99fe37448c33
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2021-02-07 19:47:36 +0100

    Validate HMAC key

--- src/Vendr.Contrib.PaymentProviders.Adyen/AdyenPaymentProviderBase.cs
+++ src/Vendr.Contrib.PaymentProviders.Adyen/AdyenPaymentProviderBase.cs
@@ -70,6 +70,7 @@ namespace Vendr.Contrib.PaymentProviders.Adyen
         protected Adyen.Model.Notification.NotificationRequestItem GetWebhookAdyenEvent(HttpRequestBase request, AdyenSettingsBase settings)
         {
             var environment = GetEnvironment(settings);
+            string hmacKey = "";
 
             Adyen.Model.Notification.NotificationRequestItem adyenEvent = null;
 
@@ -93,10 +94,8 @@ namespace Vendr.Contrib.PaymentProviders.Adyen
 
                         // TODO: Check enviroment match notification.Live ("false" or "true")
                         // Verify "hmacSignature" in AdditionalData property
-                        // Verify "orderReference" in "metadata.orderReference"
 
-                        //Adyen.Model.Notification.NotificationRequestItem
-                        //var hmacValidator = new Adyen.Util.HmacValidator();
+                        var hmacValidator = new Adyen.Util.HmacValidator();
                         //var encrypted = hmacValidator.CalculateHmac(data, key);
 
                         bool? liveMode = notification.Live.TryParse<bool>();
@@ -104,14 +103,22 @@ namespace Vendr.Contrib.PaymentProviders.Adyen
                         // Check live mode from notification is opposite of test mode setting.
                         if (liveMode.HasValue && liveMode.Value != settings.TestMode)
                         {
-                            var item = notification.NotificationItemContainers[0].NotificationItem;
+                            var notificationItem = notification.NotificationItemContainers[0].NotificationItem;
 
-                            //var hmacValidator = new Adyen.Util.HmacValidator();
-                            //var encrypted = hmacValidator.CalculateHmac(data, key);
+                            if (hmacValidator.IsValidHmac(notificationItem, hmacKey))
+                            {
+                                string eventCode = notificationItem.EventCode;
+                                // Process the notification based on the eventCode
 
-                            adyenEvent = item;
+                                adyenEvent = notificationItem;
 
-                            HttpContext.Current.Items["Vendr_AdyenEvent"] = adyenEvent;
+                                HttpContext.Current.Items["Vendr_AdyenEvent"] = adyenEvent;
+                            }
+                            else
+                            {
+                                // Non valid NotificationRequest
+                                Vendr.Log.Warn<AdyenPaymentProviderBase<TSettings>>($"Failed verifying HMAC key for {notificationItem.PspReference}.");
+                            }
                         }
                     }
                 }
