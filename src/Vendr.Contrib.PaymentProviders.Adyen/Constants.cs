eea2ad0ff06a21c0ec095a9dd00c17160dfb1201
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2021-02-11 21:45:47 +0100

    Add constants for additional data

---
+++ src/Vendr.Contrib.PaymentProviders.Adyen/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace Vendr.Contrib.PaymentProviders.Adyen
+{
+    public class Constants
+    {
+        // Adyen constants in .NET library: https://github.com/Adyen/adyen-dotnet-api-library/blob/master/Adyen/Constants/AdditionalData.cs
+        
+        public class AdditionalData
+        {
+            // Additional constant for "paymentLinkId"
+            public const string PaymentLinkId = "paymentLinkId";
+
+            // Custom constants for metadata
+            public const string OrderReference = "metadata.orderReference";
+            public const string OrderId = "metadata.orderId";
+            public const string OrderNumber = "metadata.orderNumber";
+        }
+    }
+}
