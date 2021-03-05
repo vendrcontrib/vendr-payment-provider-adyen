c85fd57549c83b95d490dc1dfbdb755dbbef2d2a
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2021-03-05 21:55:05 +0100

    Configurate locale, allowed and blocked payment methods

--- src/Vendr.Contrib.PaymentProviders.Adyen/AdyenCheckoutSettings.cs
+++ src/Vendr.Contrib.PaymentProviders.Adyen/AdyenCheckoutSettings.cs
@@ -4,9 +4,19 @@ namespace Vendr.Contrib.PaymentProviders.Adyen
 {
     public class AdyenCheckoutSettings : AdyenSettingsBase
     {
-        [PaymentProviderSetting(Name = "Accepted Payment Methods",
-            Description = "A comma separated list of Payment Methods to accept.",
+        [PaymentProviderSetting(Name = "Allowed Payment Methods",
+            Description = "A comma separated list of payment methods to be presented to the shopper.",
             SortOrder = 1000)]
-        public string PaymentMethods { get; set; }
+        public string AllowedPaymentMethods { get; set; }
+
+        [PaymentProviderSetting(Name = "Blocked Payment Methods",
+            Description = "A comma separated list of payment methods to be hidden from the shopper.",
+            SortOrder = 1100)]
+        public string BlockedPaymentMethods { get; set; }
+
+        [PaymentProviderSetting(Name = "Locale",
+            Description = "The language to be used in the payment page, specified by a combination of a language and country code.",
+            SortOrder = 1200)]
+        public string Locale { get; set; }
     }
 }
